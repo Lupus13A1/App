6b3a56f526d7be0e268e23bc880abb4695691075
Thanet <symphoniga@gmail.com>
2024-04-19 21:08:46 +0700

    comit

--- src/app/team/page.jsx
+++ src/app/team/page.jsx
@@ -138,6 +138,7 @@ export default function Team() {
               </div>
             </div>
           </div>
+
           <div>
             <div className="relative pb-56 mb-4 rounded shadow lg:pb-64">
               <Image
@@ -263,6 +264,7 @@ export default function Team() {
               </div>
             </div>
           </div>
+
           <div>
             <div className="relative pb-56 mb-4 rounded shadow lg:pb-64">
               <Image
@@ -383,6 +385,7 @@ export default function Team() {
               </div>
             </div>
           </div>
+
           <div>
             <div className="relative pb-56 mb-4 rounded shadow lg:pb-64">
               <Image
@@ -761,6 +764,246 @@ export default function Team() {
               <p className="mb-5 text-xs text-black-800">
                 Mr. Manunchai Koedpong
               </p>
+              <div className="flex items-center space-x-3 sm:justify-center">
+                <a
+                  href="#"
+                  className="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
+                >
+                  <svg
+                    x="0px"
+                    y="0px"
+                    width="35"
+                    height="35"
+                    viewBox="0 0 48 48"
+                  >
+                    <radialGradient
+                      id="yOrnnhliCrdS2gy~4tD8ma_Xy10Jcu1L2Su_gr1"
+                      cx="19.38"
+                      cy="42.035"
+                      r="44.899"
+                      gradientUnits="userSpaceOnUse"
+                    >
+                      <stop offset="0" stop-color="#fd5"></stop>
+                      <stop offset=".328" stop-color="#ff543f"></stop>
+                      <stop offset=".348" stop-color="#fc5245"></stop>
+                      <stop offset=".504" stop-color="#e64771"></stop>
+                      <stop offset=".643" stop-color="#d53e91"></stop>
+                      <stop offset=".761" stop-color="#cc39a4"></stop>
+                      <stop offset=".841" stop-color="#c837ab"></stop>
+                    </radialGradient>
+                    <path
+                      fill="url(#yOrnnhliCrdS2gy~4tD8ma_Xy10Jcu1L2Su_gr1)"
+                      d="M34.017,41.99l-20,0.019c-4.4,0.004-8.003-3.592-8.008-7.992l-0.019-20	c-0.004-4.4,3.592-8.003,7.992-8.008l20-0.019c4.4-0.004,8.003,3.592,8.008,7.992l0.019,20	C42.014,38.383,38.417,41.986,34.017,41.99z"
+                    ></path>
+                    <radialGradient
+                      id="yOrnnhliCrdS2gy~4tD8mb_Xy10Jcu1L2Su_gr2"
+                      cx="11.786"
+                      cy="5.54"
+                      r="29.813"
+                      gradientTransform="matrix(1 0 0 .6663 0 1.849)"
+                      gradientUnits="userSpaceOnUse"
+                    >
+                      <stop offset="0" stop-color="#4168c9"></stop>
+                      <stop
+                        offset=".999"
+                        stop-color="#4168c9"
+                        stop-opacity="0"
+                      ></stop>
+                    </radialGradient>
+                    <path
+                      fill="url(#yOrnnhliCrdS2gy~4tD8mb_Xy10Jcu1L2Su_gr2)"
+                      d="M34.017,41.99l-20,0.019c-4.4,0.004-8.003-3.592-8.008-7.992l-0.019-20	c-0.004-4.4,3.592-8.003,7.992-8.008l20-0.019c4.4-0.004,8.003,3.592,8.008,7.992l0.019,20	C42.014,38.383,38.417,41.986,34.017,41.99z"
+                    ></path>
+                    <path
+                      fill="#fff"
+                      d="M24,31c-3.859,0-7-3.14-7-7s3.141-7,7-7s7,3.14,7,7S27.859,31,24,31z M24,19c-2.757,0-5,2.243-5,5	s2.243,5,5,5s5-2.243,5-5S26.757,19,24,19z"
+                    ></path>
+                    <circle cx="31.5" cy="16.5" r="1.5" fill="#fff"></circle>
+                    <path
+                      fill="#fff"
+                      d="M30,37H18c-3.859,0-7-3.14-7-7V18c0-3.86,3.141-7,7-7h12c3.859,0,7,3.14,7,7v12	C37,33.86,33.859,37,30,37z M18,13c-2.757,0-5,2.243-5,5v12c0,2.757,2.243,5,5,5h12c2.757,0,5-2.243,5-5V18c0-2.757-2.243-5-5-5H18z"
+                    ></path>
+                  </svg>
+                </a>
+                <a
+                  href="/"
+                  className="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
+                >
+                  <svg
+                    x="0px"
+                    y="0px"
+                    width="30"
+                    height="30"
+                    viewBox="0 0 48 48"
+                  >
+                    <linearGradient
+                      id="CXanuwD9EGkBgTn76_1mxa_p62ASPK2Kpqp_gr1"
+                      x1="9.993"
+                      x2="40.615"
+                      y1="-299.993"
+                      y2="-330.615"
+                      gradientTransform="matrix(1 0 0 -1 0 -290)"
+                      gradientUnits="userSpaceOnUse"
+                    >
+                      <stop offset="0" stop-color="#2aa4f4"></stop>
+                      <stop offset="1" stop-color="#007ad9"></stop>
+                    </linearGradient>
+                    <path
+                      fill="url(#CXanuwD9EGkBgTn76_1mxa_p62ASPK2Kpqp_gr1)"
+                      d="M24,4C12.954,4,4,12.954,4,24c0,10.028,7.379,18.331,17.004,19.777	C21.981,43.924,22.982,41,24,41c0.919,0,1.824,2.938,2.711,2.818C36.475,42.495,44,34.127,44,24C44,12.954,35.046,4,24,4z"
+                    ></path>
+                    <path
+                      d="M27.707,21.169c0-1.424,0.305-3.121,1.757-3.121h4.283l-0.001-5.617l-0.05-0.852l-0.846-0.114	c-0.608-0.082-1.873-0.253-4.206-0.253c-5.569,0-8.636,3.315-8.636,9.334v2.498H15.06v7.258h4.948V43.6	C21.298,43.861,22.633,44,24,44c1.268,0,2.504-0.131,3.707-0.357V30.301h5.033l1.122-7.258h-6.155V21.169z"
+                      opacity=".05"
+                    ></path>
+                    <path
+                      d="M27.207,21.169c0-1.353,0.293-3.621,2.257-3.621h3.783V12.46l-0.026-0.44l-0.433-0.059	c-0.597-0.081-1.838-0.249-4.143-0.249c-5.323,0-8.136,3.055-8.136,8.834v2.998H15.56v6.258h4.948v13.874	C21.644,43.876,22.806,44,24,44c1.094,0,2.16-0.112,3.207-0.281V29.801h5.104l0.967-6.258h-6.072V21.169z"
+                      opacity=".05"
+                    ></path>
+                    <path
+                      fill="#fff"
+                      d="M26.707,29.301h5.176l0.813-5.258h-5.989v-2.874c0-2.184,0.714-4.121,2.757-4.121h3.283V12.46	c-0.577-0.078-1.797-0.248-4.102-0.248c-4.814,0-7.636,2.542-7.636,8.334v3.498H16.06v5.258h4.948v14.475	C21.988,43.923,22.981,44,24,44c0.921,0,1.82-0.062,2.707-0.182V29.301z"
+                    ></path>
+                  </svg>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <div className="relative pb-56 mb-4 rounded shadow lg:pb-64">
+              <Image
+                className="absolute object-cover w-full h-full rounded"
+                src="/img/Kittiphop.jpg"
+                alt="Kittiphop"
+                width={1160}
+                height={750}
+              />
+            </div>
+            <div className="flex flex-col sm:text-center">
+              <p className="text-lg font-bold">กิตติภพ จันทนงค์ </p>
+              <p className="mb-5 text-xs text-black-800">
+                Mr. Kittiphop Chanthanong
+              </p>
+              <div className="flex items-center space-x-3 sm:justify-center">
+                <a
+                  href="#"
+                  className="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
+                >
+                  <svg
+                    x="0px"
+                    y="0px"
+                    width="35"
+                    height="35"
+                    viewBox="0 0 48 48"
+                  >
+                    <radialGradient
+                      id="yOrnnhliCrdS2gy~4tD8ma_Xy10Jcu1L2Su_gr1"
+                      cx="19.38"
+                      cy="42.035"
+                      r="44.899"
+                      gradientUnits="userSpaceOnUse"
+                    >
+                      <stop offset="0" stop-color="#fd5"></stop>
+                      <stop offset=".328" stop-color="#ff543f"></stop>
+                      <stop offset=".348" stop-color="#fc5245"></stop>
+                      <stop offset=".504" stop-color="#e64771"></stop>
+                      <stop offset=".643" stop-color="#d53e91"></stop>
+                      <stop offset=".761" stop-color="#cc39a4"></stop>
+                      <stop offset=".841" stop-color="#c837ab"></stop>
+                    </radialGradient>
+                    <path
+                      fill="url(#yOrnnhliCrdS2gy~4tD8ma_Xy10Jcu1L2Su_gr1)"
+                      d="M34.017,41.99l-20,0.019c-4.4,0.004-8.003-3.592-8.008-7.992l-0.019-20	c-0.004-4.4,3.592-8.003,7.992-8.008l20-0.019c4.4-0.004,8.003,3.592,8.008,7.992l0.019,20	C42.014,38.383,38.417,41.986,34.017,41.99z"
+                    ></path>
+                    <radialGradient
+                      id="yOrnnhliCrdS2gy~4tD8mb_Xy10Jcu1L2Su_gr2"
+                      cx="11.786"
+                      cy="5.54"
+                      r="29.813"
+                      gradientTransform="matrix(1 0 0 .6663 0 1.849)"
+                      gradientUnits="userSpaceOnUse"
+                    >
+                      <stop offset="0" stop-color="#4168c9"></stop>
+                      <stop
+                        offset=".999"
+                        stop-color="#4168c9"
+                        stop-opacity="0"
+                      ></stop>
+                    </radialGradient>
+                    <path
+                      fill="url(#yOrnnhliCrdS2gy~4tD8mb_Xy10Jcu1L2Su_gr2)"
+                      d="M34.017,41.99l-20,0.019c-4.4,0.004-8.003-3.592-8.008-7.992l-0.019-20	c-0.004-4.4,3.592-8.003,7.992-8.008l20-0.019c4.4-0.004,8.003,3.592,8.008,7.992l0.019,20	C42.014,38.383,38.417,41.986,34.017,41.99z"
+                    ></path>
+                    <path
+                      fill="#fff"
+                      d="M24,31c-3.859,0-7-3.14-7-7s3.141-7,7-7s7,3.14,7,7S27.859,31,24,31z M24,19c-2.757,0-5,2.243-5,5	s2.243,5,5,5s5-2.243,5-5S26.757,19,24,19z"
+                    ></path>
+                    <circle cx="31.5" cy="16.5" r="1.5" fill="#fff"></circle>
+                    <path
+                      fill="#fff"
+                      d="M30,37H18c-3.859,0-7-3.14-7-7V18c0-3.86,3.141-7,7-7h12c3.859,0,7,3.14,7,7v12	C37,33.86,33.859,37,30,37z M18,13c-2.757,0-5,2.243-5,5v12c0,2.757,2.243,5,5,5h12c2.757,0,5-2.243,5-5V18c0-2.757-2.243-5-5-5H18z"
+                    ></path>
+                  </svg>
+                </a>
+                <a
+                  href="/"
+                  className="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
+                >
+                  <svg
+                    x="0px"
+                    y="0px"
+                    width="30"
+                    height="30"
+                    viewBox="0 0 48 48"
+                  >
+                    <linearGradient
+                      id="CXanuwD9EGkBgTn76_1mxa_p62ASPK2Kpqp_gr1"
+                      x1="9.993"
+                      x2="40.615"
+                      y1="-299.993"
+                      y2="-330.615"
+                      gradientTransform="matrix(1 0 0 -1 0 -290)"
+                      gradientUnits="userSpaceOnUse"
+                    >
+                      <stop offset="0" stop-color="#2aa4f4"></stop>
+                      <stop offset="1" stop-color="#007ad9"></stop>
+                    </linearGradient>
+                    <path
+                      fill="url(#CXanuwD9EGkBgTn76_1mxa_p62ASPK2Kpqp_gr1)"
+                      d="M24,4C12.954,4,4,12.954,4,24c0,10.028,7.379,18.331,17.004,19.777	C21.981,43.924,22.982,41,24,41c0.919,0,1.824,2.938,2.711,2.818C36.475,42.495,44,34.127,44,24C44,12.954,35.046,4,24,4z"
+                    ></path>
+                    <path
+                      d="M27.707,21.169c0-1.424,0.305-3.121,1.757-3.121h4.283l-0.001-5.617l-0.05-0.852l-0.846-0.114	c-0.608-0.082-1.873-0.253-4.206-0.253c-5.569,0-8.636,3.315-8.636,9.334v2.498H15.06v7.258h4.948V43.6	C21.298,43.861,22.633,44,24,44c1.268,0,2.504-0.131,3.707-0.357V30.301h5.033l1.122-7.258h-6.155V21.169z"
+                      opacity=".05"
+                    ></path>
+                    <path
+                      d="M27.207,21.169c0-1.353,0.293-3.621,2.257-3.621h3.783V12.46l-0.026-0.44l-0.433-0.059	c-0.597-0.081-1.838-0.249-4.143-0.249c-5.323,0-8.136,3.055-8.136,8.834v2.998H15.56v6.258h4.948v13.874	C21.644,43.876,22.806,44,24,44c1.094,0,2.16-0.112,3.207-0.281V29.801h5.104l0.967-6.258h-6.072V21.169z"
+                      opacity=".05"
+                    ></path>
+                    <path
+                      fill="#fff"
+                      d="M26.707,29.301h5.176l0.813-5.258h-5.989v-2.874c0-2.184,0.714-4.121,2.757-4.121h3.283V12.46	c-0.577-0.078-1.797-0.248-4.102-0.248c-4.814,0-7.636,2.542-7.636,8.334v3.498H16.06v5.258h4.948v14.475	C21.988,43.923,22.981,44,24,44c0.921,0,1.82-0.062,2.707-0.182V29.301z"
+                    ></path>
+                  </svg>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <div className="relative pb-56 mb-4 rounded shadow lg:pb-64">
+              <Image
+                className="absolute object-cover w-full h-full rounded"
+                src="/img/Thanet.jpg"
+                alt="Thanet"
+                width={1160}
+                height={750}
+              />
+            </div>
+            <div className="flex flex-col sm:text-center">
+              <p className="text-lg font-bold">ธเนตร จั่นขั้ว</p>
+              <p className="mb-5 text-xs text-black-800">Mr. Thanet Chakhua</p>
               <div className="flex items-center space-x-3 sm:justify-center">
                 <a
                   href="#"
